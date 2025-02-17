15ae36be617d8a9433d739bcfcbdd4f8f6cfc14f
Hemanth8540 <raviralahemanth@gmail.com>
2025-02-17 13:11:49 +0530

    Create SortList.java

---
+++ SortList.java
@@ -0,0 +1,28 @@
+class SortList {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode slow = head, fast = head, prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        return merge(sortList(head), sortList(slow));
+    }
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0), p = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        p.next = (l1 != null) ? l1 : l2;
+        return dummy.next;
+    }
+}
